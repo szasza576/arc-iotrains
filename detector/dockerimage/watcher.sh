4dde4ce647db98f4eed0a3cfa5e9c24046d3f9b2
Gábor Szabó <szasza576@gmail.com>
2023-09-08 14:10:13 +0200

    Add debug printouts.

--- detector/dockerimage/watcher.sh
+++ detector/dockerimage/watcher.sh
@@ -8,6 +8,7 @@ cp "${archivefolder}/${lastmasked}" "${webfolder}/masked.jpg"
 while true; do
   newfile=$(ls -Art $sourcefolder/*jpg | xargs -n 1 basename | tail -n 1)
   if [ "${sourcefolder}/${newfile}" -nt "${archivefolder}/${lastfile}" ]; then
+    date -u "+%Y%m%d-%H%M%S"
     echo "Processing new file: "$newfile
     recheck=0
     while [[ ! -s "${sourcefolder}/${newfile}" && $recheck -lt 3 ]]; do
@@ -15,13 +16,23 @@ while true; do
       ((recheck++))
     done
     if [ -s "${sourcefolder}/${newfile}" ]; then
+      date -u "+%Y%m%d-%H%M%S"
+      echo "Start the copy."
       cp --no-preserve=mode "${sourcefolder}/${newfile}" "${archivefolder}/${newfile}" # mv didn't work as it cannot preserve permissions and drops error.
+      date -u "+%Y%m%d-%H%M%S"
+      echo "Delete file
       rm "${sourcefolder}/${newfile}"
+      date -u "+%Y%m%d-%H%M%S"
+      echo "Start scoring"
       python3 masking.py "${archivefolder}/${newfile}"
+      date -u "+%Y%m%d-%H%M%S"
+      echo "Scoring done. Copy to the webfolder"
       lastfile=$(ls -Art $archivefolder/*jpg | xargs -n 1 basename | grep -v masked | tail -n 1)
       lastmasked=$(ls -Art $archivefolder/*jpg | xargs -n 1 basename | grep masked | tail -n 1)
       cp "${archivefolder}/${lastfile}" "${webfolder}/original.jpg"
       cp "${archivefolder}/${lastmasked}" "${webfolder}/masked.jpg"
+      date -u "+%Y%m%d-%H%M%S"
+      echo "Copy done."
     fi
   fi
   sleep 0.2
